c4a3d5472069c20f3c82235bd0e67745b683be17
MargFW <116837473+MargFW@users.noreply.github.com>
2023-12-12 20:41:40 -0500

    Update AOTFcontroller.ino

--- AOTFcontroller.ino
+++ AOTFcontroller.ino
@@ -1,186 +1,151 @@
-/*
- * This goal of the application is to set the digital output on pins 8-13 
- * This can be accomplished in three ways.  First, a serial command can directly set
- * the digital output pattern.  Second, a series of patterns can be stored in the 
- * Arduino and TTLs coming in on pin 2 will then trigger to the consecutive pattern (trigger mode).
- * Third, intervals between consecutive patterns can be specified and paterns will be 
- * generated at these specified time points (timed trigger mode).
- *
- * Interface specifications:
- * digital pattern specification: single byte, bit 0 corresponds to pin 8, 
- *   bit 1 to pin 9, etc..  Bits 7 and 8 will not be used (and should stay 0).
- *
- * Set digital output command: 1p
- *   Where p is the desired digital pattern.  Controller will return 1 to 
- *   indicate succesfull execution.
- *
- * Get digital output command: 2
- *   Controller will return 2p.  Where p is the current digital output pattern
- *
- * Set Analogue output command: 3xvv
- *   Where x is the output channel (either 1 or 2), and vv is the output in a 
- *   12-bit significant number.
- *   Controller will return 3xvv:
- *
- * Get Analogue output:  4
- *
- *
- * Set digital patten for triggered mode: 5xd 
- *   Where x is the number of the pattern (currently, 12 patterns can be stored).
- *   and d is the digital pattern to be stored at that position.  Note that x should
- *   be the real number (i.e., not  ASCI encoded)
- *   Controller will return 5xd 
- *
- * Set the Number of digital patterns to be used: 6x
- *   Where x indicates how many digital patterns will be used (currently, up to 12
- *   patterns maximum).  In triggered mode, after reaching this many triggers, 
- *   the controller will re-start the sequence with the first pattern.
- *   Controller will return 6x
- *
- * Skip trigger: 7x
- *   Where x indicates how many digital change events on the trigger input pin
- *   will be ignored.
- *   Controller will respond with 7x
- *
- * Start trigger mode: 8
- *   Controller will return 8 to indicate start of triggered mode
- *   Stop triggered a 9. Trigger mode will  supersede (but not stop) 
- *   blanking mode (if it was active)
- * 
- * Stop Trigger mode: 9
- *   Controller will return 9x where x is the number of triggers received during the last
- *   trigger mode run
- *
- * Set time interval for timed trigger mode: 10xtt
- *   Where x is the number of the interval (currently, 12 intervals can be stored)
- *   and tt is the interval (in ms) in Arduino unsigned int format.  
- *   Controller will return 10x
- *
-  * Sets how often the timed pattern will be repeated: 11x
- *   This value will be used in timed-trigger mode and sets how often the output
- *   pattern will be repeated. 
- *   Controller will return 11x
- *  
- * Starts timed trigger mode: 12
- *   In timed trigger mode, digital patterns as set with function 5 will appear on the 
- *   output pins with intervals (in ms) as set with function 10.  After the number of 
- *   patterns set with function 6, the pattern will be repeated for the number of times
- *   set with function 11.  Any input character (which will be processed) will stop 
- *   the pattern generation.
- *   Controller will retun 12.
- * 
- * Start blanking Mode: 20
- *   In blanking mode, zeroes will be written on the output pins when the trigger pin
- *   is low, when the trigger pin is high, the pattern set with command #1 will be 
- *   applied to the output pins. 
- *   Controller will return 20
- *
- * Stop blanking Mode: 21
- *   Stops blanking mode.  Controller returns 21
- *
- * Blanking mode trigger direction: 22x
- *   Sets whether to blank on trigger high or trigger low.  x=0: blank on trigger high,
- *   x=1: blank on trigger low.  x=0 is the default
- *   Controller returns 22
- *
- * 
- * Get Identification: 30
- *   Returns (asci!) MM-Ard\r\n
- *
- * Get Version: 31
- *   Returns: version number (as ASCI string) \r\n
- *
- * Read digital state of analogue input pins 0-5: 40
- *   Returns raw value of PINC (two high bits are not used)
- *
- * Read analogue state of pint pins 0-5: 41x
- *   x=0-5.  Returns analogue value as a 10-bit number (0-1023)
- *
- *
- * 
- * Possible extensions:
- *   Set and Get Mode (low, change, rising, falling) for trigger mode
- *   Get digital patterm
- *   Get Number of digital patterns
- */
- 
-   unsigned int version_ = 2;
-   
-   // pin on which to receive the trigger (2 and 3 can be used with interrupts, although this code does not use interrupts)
-   int inPin_ = 2;
-   // to read out the state of inPin_ faster, use 
-   int inPinBit_ = 1 << inPin_;  // bit mask 
-   
-   // pin connected to DIN of TLV5618
-   int dataPin = 3;
-   // pin connected to SCLK of TLV5618
-   int clockPin = 4;
-   // pin connected to CS of TLV5618
-   int latchPin = 5;
+#include <Servo.h>
+#include <Stepper.h>
+#include <AccelStepper.h>
 
-   const int SEQUENCELENGTH = 12;  // this should be good enough for everybody;)
-   byte triggerPattern_[SEQUENCELENGTH] = {0,0,0,0,0,0,0,0,0,0,0,0};
-   unsigned int triggerDelay_[SEQUENCELENGTH] = {0,0,0,0,0,0,0,0,0,0,0,0};
-   int patternLength_ = 0;
-   byte repeatPattern_ = 0;
-   volatile long triggerNr_; // total # of triggers in this run (0-based)
-   volatile long sequenceNr_; // # of trigger in sequence (0-based)
-   int skipTriggers_ = 0;  // # of triggers to skip before starting to generate patterns
-   byte currentPattern_ = 0;
-   const unsigned long timeOut_ = 1000;
-   bool blanking_ = false;
-   bool blankOnHigh_ = false;
-   bool triggerMode_ = false;
-   boolean triggerState_ = false;
+const int GFP = 2;
+const int RFP = 3;
+const int BF = 4;
+
+const int enPin=8;
+const int stepPin = 10;
+const int dirPin = 9;
+
+const int stepsPerRevolution = 2038;
+Stepper myStepper = Stepper(stepsPerRevolution, enPin, stepPin, dirPin, 11);
+
+unsigned int version_ = 2;
+
+int inPin_ = 2; 
+
+int dataPin = 3;
+
+int clockPin = 4;
+
+int latchPin = 5;
+
+const int SEQUENCELENGTH = 12;  // this should be good enough for everybody;)
+
+byte triggerPattern_[SEQUENCELENGTH] = {0,0,0,0,0,0,0,0,0,0,0,0};
+
+unsigned int triggerDelay_[SEQUENCELENGTH] = {0,0,0,0,0,0,0,0,0,0,0,0};
+
+int patternLength_ = 0;
+
+byte repeatPattern_ = 0;
+
+volatile long triggerNr_; // total # of triggers in this run (0-based)
+
+volatile long sequenceNr_; // # of trigger in sequence (0-based)
+
+int skipTriggers_ = 0;  // # of triggers to skip before starting to generate patterns
+
+byte currentPattern_ = 0;
+
+const unsigned long timeOut_ = 1000;
+
+bool blanking_ = false;
+
+bool blankOnHigh_ = false;
+
+bool triggerMode_ = false;
+
+boolean triggerState_ = false;
+
+int steps = 0;
  
- void setup() {
-   // Higher speeds do not appear to be reliable
-   Serial.begin(57600);
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(enPin, OUTPUT);
+  digitalWrite(enPin, HIGH);
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
   
-   pinMode(inPin_, INPUT);
-   pinMode (dataPin, OUTPUT);
-   pinMode (clockPin, OUTPUT);
-   pinMode (latchPin, OUTPUT);
-   pinMode(8, OUTPUT);
-   pinMode(9, OUTPUT);
-   pinMode(10, OUTPUT);
-   pinMode(11, OUTPUT);
-   pinMode(12, OUTPUT);
-   pinMode(13, OUTPUT);
-   
-   // Set analogue pins as input:
-   DDRC = DDRC & B11000000;
-   // Turn on build-in pull-up resistors
-   PORTC = PORTC | B00111111;
-   
-   digitalWrite(latchPin, HIGH);   
+  pinMode(inPin_, INPUT);
+  pinMode (dataPin, OUTPUT);
+  pinMode (clockPin, OUTPUT);
+  pinMode (latchPin, OUTPUT);
+  pinMode(GFP, OUTPUT);
+  pinMode(RFP, OUTPUT);
+  pinMode(BF, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+
+  myStepper.setSpeed(20);
+
+  // Set analogue pins as input:
+  DDRC = DDRC & B11000000;
+  // Turn on build-in pull-up resistors
+  PORTC = PORTC | B00111111;
+
+  digitalWrite(GFP, LOW);
+  digitalWrite(RFP, LOW);
+  digitalWrite(BF, LOW);
+  digitalWrite(latchPin, HIGH);   
  }
  
- void loop() {
-   if (Serial.available() > 0) {
-     int inByte = Serial.read();
-     switch (inByte) {
+void rotateStepper(int channel) {
+  int stepsToMove = map(channel, 1, 3, 1, stepsPerRevolution);
+  myStepper.step(stepsToMove);
+  steps = stepsToMove;
+}
+
+void resetStepper(int currentsteps) {
+  for (int i = 0; i <currentsteps; i++){
+  myStepper.step(-1);
+  }
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    int inByte = Serial.read();
+    switch (inByte) {
        
-       // Set digital output
-       case 1 :
-          if (waitForSerial(timeOut_)) {
-            currentPattern_ = Serial.read();
-            // Do not set bits 6 and 7 (not sure if this is needed..)
-            currentPattern_ = currentPattern_ & B00111111;
-            if (!blanking_)
-              PORTB = currentPattern_;
-            Serial.write( byte(1));
-          }
-          break;
+      // Set digital output
+      case 1 :
+        if (waitForSerial(timeOut_)) {
+          currentPattern_ = Serial.read();
+          // Do not set bits 6 and 7 (not sure if this is needed..)
+          currentPattern_ = currentPattern_ & B00111111;
+          if (!blanking_)
+            PORTB = currentPattern_;
+            if (currentPattern_ == 1) { //BF
+            resetStepper(steps);
+            digitalWrite(GFP, LOW);
+            digitalWrite(RFP, LOW);
+            digitalWrite(BF, LOW);
+            digitalWrite(BF, HIGH);
+            rotateStepper(currentPattern_);
+            }
+            else if (currentPattern_ == 2) { //GFP
+            resetStepper(steps);
+            digitalWrite(GFP, LOW);
+            digitalWrite(RFP, LOW);
+            digitalWrite(BF, LOW);
+            digitalWrite(GFP, HIGH);
+            rotateStepper(currentPattern_);
+            }
+            else if (currentPattern_ == 3) { //RFP
+            resetStepper(steps);
+            digitalWrite(GFP, LOW);
+            digitalWrite(RFP, LOW);
+            digitalWrite(BF, LOW);
+            digitalWrite(RFP, HIGH);
+            rotateStepper(currentPattern_);
+            }
+          Serial.write( byte(1));
+        }
+        break;
           
-       // Get digital output
-       case 2:
-          Serial.write( byte(2));
-          Serial.write( PORTB);
-          break;
+      // Get digital output
+      case 2:
+        Serial.write( byte(2));
+        Serial.write( PORTB);
+        break;
           
        // Set Analogue output (TODO: save for 'Get Analogue output')
-       case 3:
+      case 3:
          if (waitForSerial(timeOut_)) {
            int channel = Serial.read();
            if (waitForSerial(timeOut_)) {
@@ -376,42 +341,6 @@
 
        }
     }
-    
-    // In trigger mode, we will blank even if blanking is not on..
-    if (triggerMode_) {
-      boolean tmp = PIND & inPinBit_;
-      if (tmp != triggerState_) {
-        if (blankOnHigh_ && tmp ) {
-          PORTB = 0;
-        }
-        else if (!blankOnHigh_ && !tmp ) {
-          PORTB = 0;
-        }
-        else { 
-          if (triggerNr_ >=0) {
-            PORTB = triggerPattern_[sequenceNr_];
-            sequenceNr_++;
-            if (sequenceNr_ >= patternLength_)
-              sequenceNr_ = 0;
-          }
-          triggerNr_++;
-        }
-        
-        triggerState_ = tmp;       
-      }  
-    } else if (blanking_) {
-      if (blankOnHigh_) {
-        if (! (PIND & inPinBit_))
-          PORTB = currentPattern_;
-        else
-          PORTB = 0;
-      }  else {
-        if (! (PIND & inPinBit_))
-          PORTB = 0;
-        else  
-          PORTB = currentPattern_;
-      }
-    }
 }
 
  
@@ -442,41 +371,6 @@ void analogueOut(int channel, byte msb, byte lsb)
   digitalWrite(clockPin, LOW);
   digitalWrite(latchPin, HIGH);
 }
-
-
-
-/* 
- // This function is called through an interrupt   
-void triggerMode() 
-{
-  if (triggerNr_ >=0) {
-    PORTB = triggerPattern_[sequenceNr_];
-    sequenceNr_++;
-    if (sequenceNr_ >= patternLength_)
-      sequenceNr_ = 0;
-  }
-  triggerNr_++;
-}
-
-
-void blankNormal() 
-{
-    if (DDRD & B00000100) {
-      PORTB = currentPattern_;
-    } else
-      PORTB = 0;
-}
-
-void blankInverted()
-{
-   if (DDRD & B00000100) {
-     PORTB = 0;
-   } else {     
-     PORTB = currentPattern_;  
-   }
-}   
-
-*/
   
 
 
